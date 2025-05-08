30df8c9a0866e33af1b96abaa6a5e0d9fd62e9ea
avicenan <avicena.n@gmail.com>
2025-05-09 00:07:13 +0700

    remove all

--- src/pages/Violation/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Violations() {
-  return <div>violations</div>;
-}
--- src/pages/Violation/show.tsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function Violation() {
-  const { violationId } = useParams();
-  return <div>violation {violationId}</div>;
-}
